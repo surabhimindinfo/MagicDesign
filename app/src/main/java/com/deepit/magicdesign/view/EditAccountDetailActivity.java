fd7f14b1aca5852318e7b2647fea14d2cc89752e
swapnil-webvillee <swapnil.webvillee@gmail.com>
2021-04-06 23:21:59 +0530

    Commit MainScreen

--- app/src/main/java/com/deepit/magicdesign/view/EditAccountDetailActivity.java
+++ app/src/main/java/com/deepit/magicdesign/view/EditAccountDetailActivity.java
@@ -1,8 +1,8 @@
 package com.deepit.magicdesign.view;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.deepit.magicdesign.R;
 
@@ -12,5 +12,10 @@ public class EditAccountDetailActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_account_detail);
+    }
+
+    public void openMainPage(View view) {
+        startActivity(new Intent(EditAccountDetailActivity.this, MainActivity.class));
+        finish();
     }
 }
--- app/src/main/java/com/deepit/magicdesign/view/MainActivity.java
+++ app/src/main/java/com/deepit/magicdesign/view/MainActivity.java
@@ -1,16 +1,85 @@
 package com.deepit.magicdesign.view;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.deepit.magicdesign.R;
+import com.deepit.magicdesign.adapter.SliderAdapter;
+import com.deepit.magicdesign.model.SliderData;
+import com.smarteist.autoimageslider.SliderView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    String url1 = "https://www.geeksforgeeks.org/wp-content/uploads/gfg_200X200-1.png";
+    String url2 = "https://qphs.fs.quoracdn.net/main-qimg-8e203d34a6a56345f86f1a92570557ba.webp";
+    String url3 = "https://bizzbucket.co/wp-content/uploads/2020/08/Life-in-The-Metro-Blog-Title-22.png";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_launch);
+        setContentView(R.layout.activity_main);
+        assert getSupportActionBar()!=null;
+
+
+        ActionBar actionBar =  getSupportActionBar();
+
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayHomeAsUpEnabled(false);
+        actionBar.setDisplayUseLogoEnabled(false);
+        actionBar.setDisplayShowCustomEnabled(true);
+
+        ActionBar.LayoutParams layoutParams = new ActionBar.LayoutParams(
+                ActionBar.LayoutParams.MATCH_PARENT,
+                ActionBar.LayoutParams.MATCH_PARENT
+        );
+
+        View view = LayoutInflater.from(this).inflate(R.layout.custom_actionbar, null);
+        actionBar.setCustomView(view, layoutParams);
+
+       Toolbar toolbar= (Toolbar) actionBar.getCustomView().getParent();
+        toolbar.setContentInsetsAbsolute(0,0);
+
+        setSliderView();
+    }
+
+    private void setSliderView() {
+        ArrayList<SliderData> sliderDataArrayList = new ArrayList<>();
+
+        // initializing the slider view.
+        SliderView sliderView = findViewById(R.id.slider);
+
+        // adding the urls inside array list
+        sliderDataArrayList.add(new SliderData(url1));
+        sliderDataArrayList.add(new SliderData(url2));
+        sliderDataArrayList.add(new SliderData(url3));
+
+        // passing this array list inside our adapter class.
+        SliderAdapter adapter = new SliderAdapter(this, sliderDataArrayList);
+
+        // below method is used to set auto cycle direction in left to
+        // right direction you can change according to requirement.
+        sliderView.setAutoCycleDirection(SliderView.LAYOUT_DIRECTION_LTR);
+
+        // below method is used to
+        // setadapter to sliderview.
+        sliderView.setSliderAdapter(adapter);
+
+        // below method is use to set
+        // scroll time in seconds.
+        sliderView.setScrollTimeInSec(3);
+
+        // to set it scrollable automatically
+        // we use below method.
+        sliderView.setAutoCycle(true);
+
+        // to start autocycle below method is used.
+        sliderView.startAutoCycle();
     }
 }
